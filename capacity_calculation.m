e3e207d717de5e8e38093d68a83bdd6248720789
frno0254 <smallgreycreatures@gmail.com>
2017-12-05 17:27:02 +0100

    updated capacity

--- capacity_calculation.m
+++ capacity_calculation.m
@@ -1,4 +1,4 @@
-function [ c_prime ] = capacity_calculation(R_amp, w, L, M)
+function [ ret] = capacity_calculation(R_amp, w, L, M)
 
 c_sek = 1/(L*w*w*(1-(M/L)));
 z_s = j*w*(L-M);
@@ -12,5 +12,8 @@ z_eq = (((z_s + (1/(j*w*c_sek)) + r)*z_m)/(z_s + (1/(j*w*c_sek)) + r + z_m)) + z
 
 c_prime = 1/(w*imag(z_eq));
 c_prime = double(c_prime);
+r = double(r);
+c_sek = double(c_sek);
+ret = [c_prime, r, c_sek];
 end
 
